ca696aa925a9953efeac9e79e49679132db0e3c9
talalaite <justina.talalaite@gmail.com>
2021-07-15 03:56:31 +0300

    auth fixed

--- src/middleware.js
+++ src/middleware.js
@@ -4,7 +4,7 @@ const { jwtSecret } = require("./config");
 module.exports = {
   loggedIn: (req, res, next) => {
     try {
-      const token = req.headers.authorization?.split(" ")[1];
+      const token = req.headers.authorization.split(" ")[1];
       const decodedToken = jwt.verify(token, jwtSecret);
       req.userData = decodedToken;
       next();
